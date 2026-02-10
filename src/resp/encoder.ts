86f31e7922f3112ad4208f8b35c6ce7b6ab47d5c
Prince Jain <pj122333444@gmail.com>
2026-02-10 15:21:53 +0530

    connected to redis-cli

--- src/resp/encoder.ts
+++ src/resp/encoder.ts
@@ -2,6 +2,14 @@ export function encodeRESP(value: any): Buffer {
   if (typeof value === "string") {
     return Buffer.from(`+${value}\r\n`);
   }
+  if (value === null) {
+    return Buffer.from(`$-1\r\n`);
+  }
+
+   if (value !== null && typeof value === "object" && value.type === "bulk") {
+    const str = value.value;
+    return Buffer.from(`$${Buffer.byteLength(str)}\r\n${str}\r\n`);
+  }
 
   throw new Error("Unsupported RESP type");
 }
--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,62 @@
 import * as net from "net";
-import { Socket } from 'net';
-import {decodeRESP} from './resp/decoder';
+import { Socket } from "net";
+import { decodeRESP } from "./resp/decoder";
 import { encodeRESP } from "./resp/encoder";
+import { setKey, getKey } from "./store/memory";
 
-const port: number= 6379;
+const port: number = 6379;
 
-const server = net.createServer((socket : Socket) =>{
-    console.log('Client connected');
+const server = net.createServer((socket: Socket) => {
+  console.log("Client connected");
 
-    let buffer = Buffer.alloc(0);
+  let buffer = Buffer.alloc(0);
 
-    socket.on('data', (chunck : Buffer) =>{
-        buffer = Buffer.concat([buffer, chunck]);
+  socket.on("data", (chunck: Buffer) => {
+    buffer = Buffer.concat([buffer, chunck]);
+    console.log("Received data:", buffer.toString("utf-8"));
+    while (true) {
+      const result = decodeRESP(buffer);
+      if (!result) break;
 
-        while(true){
-            const result  = decodeRESP(buffer);
-            if (!result) break;
+      const [rawCommand, ...args] = result.value;
+      const command = rawCommand.toUpperCase();
 
-            const command = result.value[0];
 
-            console.log("Parsed command:", command);
+      console.log("Parsed command:", command);
 
-            if(command === "PING"){
-                const response = encodeRESP("PONG");
-                socket.write(response);
-            }
+      if (command === "PING") {
+        const response = encodeRESP("PONG");
+        socket.write(response);
+      } else if (command === "SET") {
+        const [key, value] = args;
+        setKey(key, value);
+        socket.write(encodeRESP("OK"));
+      } else if (command === "GET") {
+        const [key] = args;
+        const value = getKey(key);
 
-            buffer = buffer.slice(result.bytesConsumed);
+        if (value === null) {
+          socket.write(encodeRESP(null));
+        } else {
+          socket.write(encodeRESP({ type: "bulk", value }));
         }
-    })
+      } else if (command === "COMMAND") {
+        socket.write(encodeRESP({ type: "bulk", value: "" }));
+      }
 
-    socket.on('end', () =>{
-        console.log('Client disconnected');
-    })
+      buffer = buffer.slice(result.bytesConsumed);
+    }
+  });
 
-    socket.on('error', (err : Error) =>{
-        console.error('Socket error:', err);
-    })
+  socket.on("end", () => {
+    console.log("Client disconnected");
+  });
 
-})
+  socket.on("error", (err: Error) => {
+    console.error("Socket error:", err);
+  });
+});
 
-server.listen(port,() =>{
-    console.log(`Server is listening on port ${port}`);
-})
+server.listen(6379, () => {
+  console.log("Server is listening on port 6379");
+});
